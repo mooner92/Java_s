eafaba8b23ed2af611976f5981f0a909f4f9e59f
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-19 14:07:21 +0900

    add

--- hw_129_13.java
+++ hw_129_13.java
@@ -1,10 +1,8 @@
 public class hw_129_13 {
     public static void main(String[] args) {
-        String[] kinds = { "Clubs", "Diamonds", "Hearts", "Spades" };
-        String[] num = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-        for (int i = 0; i < 5; i++) {
-            System.out.println(kinds[(int) (Math.random() * 100) % 4] + "의 " + num[(int) (Math.random() * 100) % 12]);
-        }
-
+        String[][] kinds = { { "Clubs", "Diamonds", "Hearts", "Spades" },
+                                { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" } };
+        for (int i = 0; i < 5; i++) 
+            System.out.println(kinds[0][(int) (Math.random() * 100) % 4] + "의 " + kinds[1][(int) (Math.random() * 100) % 12]);
     }
 }
