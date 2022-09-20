601d6f5f5cb97e0248469d78a40a1d6fd46bd202
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-20 14:13:00 +0900

    add

---
+++ lec_n_class.java
@@ -0,0 +1,13 @@
+public class lec_n_class {
+    class X {
+        int i;
+
+        X() {
+            this.i = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        X x1 = new X();
+    }
+}
