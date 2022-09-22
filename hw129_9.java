5edd9adcac54ad47e5d8baf88568f6944f0e0251
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-22 09:21:19 +0900

    add

--- hw129_9.java
+++ hw129_9.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class hw129_9 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("출력할 항의 개수 : ");
@@ -11,12 +10,10 @@ public class hw129_9 {
         }
         sc.close();
     }
-
     public static int fibo(int n) {
         if (n <= 1)
             return n;
         else
             return fibo(n - 2) + fibo(n - 1);
     }
-    
 }
--- lec_n_class.java
+++ lec_n_class.java
@@ -1,13 +1,16 @@
-public class lec_n_class {
-    class X {
-        int i;
+class X {
+        public int i;
 
         X() {
             this.i = 0;
         }
     }
 
+public class lec_n_class {
+    
+
     public static void main(String[] args) {
         X x1 = new X();
+        System.out.println(x1.i);
     }
 }
