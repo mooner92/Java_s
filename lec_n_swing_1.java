b626dccb1b637e1c5c7787947d29a96710fccce9
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-23 10:07:37 +0900

    add

---
+++ lec_n_swing_1.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class lec_n_swing_1 {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("FrameTest");
+        f.setSize(200, 300);
+        
+    }
+}//
