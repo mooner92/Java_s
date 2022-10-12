9985e282b7b30a04e9cd9e0abb7ac74136a6d215
Mooner92 <chlaudgjs0989@gmail.com>
2022-10-13 03:23:16 +0900

    add

--- hw_n_8.java
+++ hw_n_8.java
@@ -1,4 +1,3 @@
-
 interface GetInfo {
     double getValue();
 }
@@ -7,26 +6,31 @@ class Student implements GetInfo {
     double score;
 
     Student() {
-        score = (int) Math.random() * 4.5;
+        this.score = Math.random() * 4.5;
     }
 
-    double getValue() {
+    public double getValue() {
         return this.score;
     }
 }
 
-
 public class hw_n_8 {
-    double average(double[] object) {
+
+    static double average(Student[] object) {
         double sum = 0.0;
-        for (int i = 0; i < object.length(); i++) {
+        for (int i = 0; i < object.length; i++) {
             sum += object[i].getValue();
         }
-        return sum / object.length();
+        return sum / object.length;
     }
 
     public static void main(String[] args) {
-        Student[] stl = new Student[100];
-        System.out.println(average(stl));
+        final int c = 10;  //n명
+        Student[] stl = new Student[c];
+        for (int i = 0; i < c; i++) {
+            Student st = new Student();
+            stl[i] = st;
+        }
+        System.out.printf("%.1f",average(stl));
     }
 }
