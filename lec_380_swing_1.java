05a64a70637f447e4604e7e24554878c5a971cad
Mooner92 <chlaudgjs0989@gmail.com>
2022-10-27 15:08:22 +0900

    add

--- lec_380_swing_1.java
+++ lec_380_swing_1.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
+
+import java.awt.Color;
 import java.awt.FlowLayout;
 public class lec_380_swing_1 extends JFrame{
     public lec_380_swing_1(){
         setSize(300,200);
         setTitle("MyFrame");
         setLayout(new FlowLayout());
-        JButton button = new JButton("button");
-        JLabel lable = new JLabel("안녕하세요.");
-        add(button);
+        JLabel lable = new JLabel("자바는 재미있나요?");
+        JButton button = new JButton("yes");
+        JButton button1 = new JButton("no");
+        
         add(lable).setLocation(280, 170); //작동안됨
+        add(button);
+        add(button1);
+        getContentPane().setBackground(Color.yellow);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
